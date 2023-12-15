44abc02cfca9d135fa0ed615bdaec7657590e9e4
Vitor <catchtherainbow33@gmail.com>
2023-12-14 23:37:32 -0300

    Add configs to window start screen size

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { LogicalPosition, appWindow, currentMonitor } from '@tauri-apps/api/window';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from './pages/Home.tsx';
+
+currentMonitor().then(monitor => {
+  appWindow.setPosition( new LogicalPosition(-7, 0))
+  appWindow.setSize({
+    type:'Physical',
+    width: monitor?.size.width || 0,
+    height: monitor?.size.height || 0
+  })
+});
 
 function App() {
   const rounter = createBrowserRouter([
