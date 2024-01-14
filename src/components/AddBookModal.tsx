5acf5ab809d1f54ca3005fd5a6f4c301f6413558
Vitor <catchtherainbow33@gmail.com>
2024-01-14 18:04:34 -0300

    Add style to modal

--- src/components/AddBookModal.tsx
+++ src/components/AddBookModal.tsx
@@ -1,37 +1,48 @@
-import React from 'react'
+import XMarkWhite from '../assets/x-mark-white.svg'
+import XMarkBlack from '../assets/x-mark-black.svg'
+import SelectionIcon from '../assets/selection-icon.svg'
+import MoreIcon from '../assets/more-icon.svg'
+import ConfirmIcon from '../assets/confirm-icon.svg'
+import TrashIcon from '../assets/trash-icon.svg'
+import '../styles/AddBookModal.css'
 
 const AddBookModal = () => {
   return (
-    <div>
+    <div className='modal-container'>
       <div>
-        <div>
-          <label htmlFor="">Nome:</label>
-          <input type="text" name="" id="" />
+        <div className='modal-inputs-container'>
+          <label htmlFor="name-input">Nome:</label>
+          <input type="text" name="name-input" id="name-input" />
         </div>
-        <div>
+        <div className='modal-inputs-container'>
           <h2>Arquivo:</h2>
-          <span>adfaffa.pdf</span>
-          <button>Carregar</button>
+          <span>adfaffasaaaaaaaaa.pdf</span>
+          <button className='modal-button'>Carregar</button>
         </div>
         <h2>Categorias:</h2>
-        <div>
+        <div className='modal-categ-container'>
           <ul>
-            <li>Categoria <button></button></li>
+            <li className='modal-categ'>Categoria <button><img src={XMarkWhite} alt="" /></button></li>
+
           </ul>
-          <button>Selecionar categoria</button>
-          <div>
-            <input type="text" name="" id="" placeholder='Criar categoria' />
-            <button>+</button>
+          <div className='categ-buttons-container'>
+            <button className='modal-button' >Selecionar categoria <img src={SelectionIcon} alt="" /></button>
+            <div className='modal-button create-categ'>
+              <input type="text" name="" id="" placeholder='Criar categoria' />
+              <button><img src={MoreIcon} alt="" /></button>
+            </div>
+            <button className='modal-button'>Remover categoria <img src={SelectionIcon} alt="" /></button>
+            </div>
           </div>
-          <button>Remover categoria</button>
-        </div>
       </div>
       <div>
-        <img src="" alt="" />
-        <button>Adicionar thumb</button>
+        <img className='modal-image' src="" />
+        <button className='modal-button center'>Adicionar thumb</button>
       </div>
-      <ul>
-        <li></li>
+      <ul className='modal-menu'>
+        <li><img src={XMarkBlack} alt="" /></li>
+        <li><img src={ConfirmIcon} alt="" /></li>
+        <li><img src={TrashIcon} alt="" /></li>
       </ul>
     </div>
   )
