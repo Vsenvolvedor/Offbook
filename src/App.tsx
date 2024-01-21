7a6a999d6b54a8ca15c170be4342dbb1f81f6ea7
Vitor <catchtherainbow33@gmail.com>
2024-01-20 23:11:24 -0300

    Check if data directories exist and create if no

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,22 @@ import React from "react";
 import { LogicalPosition, appWindow, currentMonitor } from '@tauri-apps/api/window';
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Home } from './pages/Home.tsx';
+import { BaseDirectory, createDir, exists } from "@tauri-apps/api/fs";
+
+exists('data\\arquives', {dir: BaseDirectory.AppData}).then((res) => {
+  if(res) return;
+  createDir('data\\arquives', {
+    dir: BaseDirectory.AppData,
+    recursive: true
+  });
+});
+exists('data\\json', {dir: BaseDirectory.AppData}).then((res) => {
+  if(res) return;
+  createDir('data\\json', {
+    dir: BaseDirectory.AppData,
+    recursive: true
+  });
+});
 
 currentMonitor().then(monitor => {
   appWindow.setPosition( new LogicalPosition(-7, 0))
