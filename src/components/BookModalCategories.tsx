c754d13b732ee1dce0696f196dd3f5071673f8a0
Vitor <catchtherainbow33@gmail.com>
2024-01-21 16:58:07 -0300

    Save data from categories and logic for the modal works

--- src/components/BookModalCategories.tsx
+++ src/components/BookModalCategories.tsx
@@ -1,46 +1,80 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/AddBookModal.css'
 import SelectionIcon from '../assets/selection-icon.svg'
 import MoreIcon from '../assets/more-icon.svg'
 import XMarkWhite from '../assets/x-mark-white.svg'
-
-const categorias = [
-  'asdad',
-  'asdasd',
-  '657cg jkjnoi'
-]
-
+import { BaseDirectory, exists, readTextFile, writeFile } from '@tauri-apps/api/fs'
 interface BookModalCategories {
   selectedCategories: string[]
   setSelectedCategories: (value:string[]) => void
 }
 
-const BookModalCategories = ({}:BookModalCategories) => {
+const BookModalCategories = ({selectedCategories,setSelectedCategories}:BookModalCategories) => {
   const [categories,setCategories] = useState<Array<string>>([]);
   const [newCategorie, setNewCategorie] = useState<string>('');
+  const categorieDataPath = 'data\\json\\categories.json';
+
+  async function readingCategoriesData() {
+    const response = await exists(categorieDataPath, {
+      dir: BaseDirectory.AppData
+    })
+    if(!response) return null;
+    const contents = await readTextFile(categorieDataPath, {
+      dir: BaseDirectory.AppData
+    });
+    return contents;
+  }
+
+  async function createCategorie() {
+    if(newCategorie ===  '') return;
+    const data = await readingCategoriesData();
+    if(data) {
+      const parsedData = JSON.parse(data);
+      parsedData.push(newCategorie);
+      await writeFile(categorieDataPath,JSON.stringify(parsedData),{dir:BaseDirectory.AppData})
+      setCategories(parsedData);
+      setNewCategorie('');
+    } else {
+      const newListCategs = [];
+      newListCategs.push(newCategorie);
+      await writeFile(categorieDataPath,JSON.stringify(newListCategs),{dir:BaseDirectory.AppData})
+      setNewCategorie('');
+    }
+  }
+
+  async function deleteCategorie({target}:any) {
+    if(target.value === '') return;
+    const data = await readingCategoriesData();
+    if(!data) return;
+    const parsetData = JSON.parse(data);
+    const deletedItemList = parsetData.filter((item:string) => item !== target.value);
+    await writeFile(categorieDataPath,JSON.stringify(deletedItemList),{dir:BaseDirectory.AppData})
+    setNewCategorie('');
+    setCategories(deletedItemList);
+  }
 
   function handleNewCategorie({target}:any){
     setNewCategorie(target.value);
   }
 
-  function createCategorie() {
-    setCategories(curr => [...curr,newCategorie])
-  }
-
   function selectCategorie({target}:any) {
     if(target.value === '') return;
-    setCategories(curr => [...curr, target.value]);
+    setSelectedCategories([...selectedCategories, target.value]);
   }
 
   function removeCategorie(index:number) {
-    const newCategorie = categories.filter((i,oldIndex) =>  oldIndex !== index);
-    setCategories(newCategorie);
+    const newCategorie = selectedCategories.filter((i,oldIndex) =>  oldIndex !== index);
+    setSelectedCategories(newCategorie);
   }
 
-  function deleteCategorie({target}:any) {
-    if(target.value === '') return;
-    setCategories(categories.filter(categ => categ !== target.value));
-  }
+  useEffect(() => {
+    readingCategoriesData()
+    .then((data) => {
+      if(!data) return;
+      setCategories(JSON.parse(data));
+    })
+  }, [])
+
 
   return (
     <>
@@ -48,16 +82,16 @@ const BookModalCategories = ({}:BookModalCategories) => {
         <div className='modal-categ-container'>
           <ul>
             {
-              categories.map((categ, index) => (
+              selectedCategories.map((categ, index) => (
                 <li key={index} className='modal-categ'>{categ}<button onClick={() => removeCategorie(index)}><img src={XMarkWhite} alt="" /></button></li>
               ))
             }
           </ul>
           <div className='categ-buttons-container'>
             <select className='modal-button' onChange={selectCategorie} name="select-categ" id="select-categ">
-              <option value="">Selecionar categoria<img src={SelectionIcon} alt="" /></option>
+              <option value="" >Selecionar categoria<img src={SelectionIcon} alt="" /></option>
               {
-                categorias.map((categ,index) => {
+                categories.map((categ,index) => {
                   return (
                     <option key={index} value={categ}>{categ}</option>
                   )
@@ -72,7 +106,7 @@ const BookModalCategories = ({}:BookModalCategories) => {
             <select onChange={deleteCategorie} className='modal-button' name="" id="">
               <option value="">Remover categoria <img src={SelectionIcon} alt="" /></option>
               {
-                categorias.map((categ,index) => {
+                categories.map((categ,index) => {
                   return (
                     <option key={index} value={categ}>{categ}</option>
                   )
