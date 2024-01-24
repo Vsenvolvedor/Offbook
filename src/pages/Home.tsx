9b2c55cbe2f18ed7ad828b67ea6a8bcb5c2ad806
Vitor <catchtherainbow33@gmail.com>
2024-01-24 19:26:53 -0300

    Create context and values

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,70 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Header } from '../components/Header'
 import Books from '../components/Books'
 import AddBookModal from '../components/AddBookModal';
+import readingBookData from '../helper/readBookData';
+import readingCategoriesData from '../helper/readCategoryData';
+
+export type BookData = {
+  id: number
+  name: string
+  source: string
+  thumb: string
+  categories: string[] 
+  fixed: boolean
+}
+
+interface ContextTypes {
+  categories: string[]
+  books: BookData[] 
+  setCategories: (value:string[])=> void
+  setBooks: (value:BookData[])=> void
+}
+
+export const AppDataContext = React.createContext<ContextTypes>({
+  categories: [''],
+  setCategories: () => {},
+  books: [],
+  setBooks: () => {}
+});
 
 export const Home = () => {
+  const [books, setBooks] = useState<BookData[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
   const [isModalActive, setIsModalActive] = useState<boolean>(false);
+
+  useEffect(() => {
+    async function loadingData() {
+      const dataBooks = await readingBookData();
+      const dataCategories = await readingCategoriesData();
+      if(dataBooks) {
+        setBooks(JSON.parse(dataBooks));
+      }
+      if(dataCategories) {
+        setCategories(JSON.parse(dataCategories));
+      }
+    }
+
+    loadingData();
+  },[])
+
   return (
-    <div>
-      <Header 
-        setIsModalActive={setIsModalActive}
-      />
-      <Books 
-        isModalActive={isModalActive}
-      />
-      {isModalActive && <AddBookModal setIsModalActive={setIsModalActive} />}
-    </div>
+    <AppDataContext.Provider value={{
+      categories,
+      setCategories,
+      books,
+      setBooks
+    }}>
+      <div>
+        <Header 
+          setIsModalActive={setIsModalActive}
+        />
+        <Books 
+          isModalActive={isModalActive}
+        />
+        {isModalActive && <AddBookModal setIsModalActive={setIsModalActive} />}
+      </div>
+    </AppDataContext.Provider>
+
   )
 }
