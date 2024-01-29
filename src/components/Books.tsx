230e3c0ac72189bbccda879594930bbd7f400598
Vitor <catchtherainbow33@gmail.com>
2024-01-29 20:43:51 -0300

    Add filter function to books component

--- src/components/Books.tsx
+++ src/components/Books.tsx
@@ -1,22 +1,37 @@
-import React from 'react'
+import { useContext, useMemo } from 'react'
 import Book from './Book'
 import "../styles/Books.css";
+import { AppDataContext, BookData } from '../pages/Home';
 
 interface BooksInterface {
   isModalActive: boolean
 }
 
 const Books = (props:BooksInterface) => {
+  const {books} = useContext(AppDataContext);
+  const classifiedBooks = useMemo<BookData[]>(() => {
+    const newList:BookData[] = [];
+    books.forEach((book) => {
+      if(book.fixed) {
+        newList.unshift(book);
+      } else {
+        newList.push(book);
+      }
+    })
+    return newList;
+  }, [books]);
+
   return (
     <ul className={`books-container ${props.isModalActive && "blur-background"}`}>
-      <li>
-        <Book />
-      </li>
-      <li>
-        <Book />
-      </li>
-      
+      {
+        classifiedBooks.map((book) => {
+          return (
+            <Book key={book.id} data={book} />
+          )
+        })
+      }
     </ul>
+
   )
 }
 
