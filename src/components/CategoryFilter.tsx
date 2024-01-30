015708a673cf7728a030b6f74306c4d203fa121a
Vitor <catchtherainbow33@gmail.com>
2024-01-29 21:57:18 -0300

    Add search by categories

---
+++ src/components/CategoryFilter.tsx
@@ -0,0 +1,46 @@
+import { useContext, useMemo, useRef } from 'react'
+import { AppDataContext } from '../pages/Home'
+import '../styles/Header.css';
+
+interface CategoryFilter {
+  category: string
+}
+
+const CategoryFilter = ({category}:CategoryFilter) => {
+  const {books,setBooks} = useContext(AppDataContext);
+  const categRef = useRef<HTMLLIElement | null>(null);
+  const originalBooks = useMemo(()=>{
+    return books.map(book => book);
+  },[])
+
+  function filterByCategory() {
+    if(categRef.current?.classList.contains('categ-active')) {
+      categRef.current?.classList.remove('categ-active');
+      setBooks(originalBooks);
+      return;
+    };
+    const filteredList = books.filter((book) => {
+      const hasCategory = book.categories.reduce<boolean>((acc, categ) => {
+        if(categ === category || acc) {
+          acc = true;
+          return true;
+        } else {
+          acc = false;
+          return false;
+        }
+      }, false)
+      
+      return hasCategory;
+    });
+    categRef.current?.classList.add('categ-active')
+    setBooks(filteredList);
+  }
+
+  return (
+    <li onClick={filterByCategory} ref={categRef}>
+      {category}
+    </li>
+  )
+}
+
+export default CategoryFilter
