857d1de6727e2e80a3b2adc778bd13a23c1bc9ae
Vitor <catchtherainbow33@gmail.com>
2024-01-29 21:06:55 -0300

    Add search function to search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,10 +1,33 @@
+import { useContext, useEffect, useMemo, useState } from 'react';
 import SearchIcon from '../assets/search-icon.svg';
+import { AppDataContext } from '../pages/Home';
 
 const Search = () => {
+  const {books,setBooks} = useContext(AppDataContext);
+  const [searchText, setSearchText] = useState<string>('');
+  const originalBooks = useMemo(() => {
+    return books.map((book)=> book);
+  }, [])
+
+  useEffect(() => {
+    if(searchText === '') {
+      setBooks(originalBooks);
+      return;
+    };
+    const searchedBooks = books.filter((book) => {
+      return book.name.includes(searchText);
+    });
+    setBooks(searchedBooks);
+  }, [searchText])
+
+  function handleChange({target}:any) {
+    setSearchText(target.value);
+  };
+
   return (
     <div className='search-container'>
       <img id='search-button' src={SearchIcon} alt="Search button" />
-      <input type="search" name="" id="search-input" placeholder='Pesquise seu arquivo aqui' />
+      <input onChange={handleChange} type="search" id="search-input" value={searchText} placeholder='Pesquise seu arquivo aqui' />
     </div>
   )
 }
