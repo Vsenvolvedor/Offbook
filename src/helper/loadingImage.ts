88fcaec80a38f391179cc0dfcfeda297062b4b9d
Vitor <catchtherainbow33@gmail.com>
2024-01-30 22:01:10 -0300

    Add new function to load and use images in app

---
+++ src/helper/loadingImage.ts
@@ -0,0 +1,9 @@
+import { BaseDirectory, readBinaryFile } from "@tauri-apps/api/fs";
+import { BookData } from "../pages/Home";
+
+export default async function loadingImage(data:BookData) {
+  const imageUnit8ArrayData = await readBinaryFile(data.thumb,{dir: BaseDirectory.AppData});
+  const imageBlob = new Blob([imageUnit8ArrayData.buffer]);
+  const imageUrl = URL.createObjectURL(imageBlob);
+  return imageUrl;
+};
